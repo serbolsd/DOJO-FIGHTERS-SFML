acdc26812e6e38c02f0619305029917de9a4d051
Chongnaitor <idv18c.jchong@uartesdigitales.edu.mx>
2019-07-27 17:28:01 -0500

    Objetive Class
    Miembros nada mas

---
+++ DOJO-FIGHTERS-SFML/DOJO-FIGHTERS-SFML/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include"Graphics.hpp"
+class  Objetive {
+	 float Life;
+	 sf::Vector2f Direccion;
+	 //QuadCollider Collider;
+	 //QuadCollider AttackCollider
+	 sf::Vector2f Position;
+
+
+};
