498c47c9b8c0e08ab6295722cac1db170d6e0242
serbolsd <32022302+serbolsd@users.noreply.github.com>
2019-07-27 16:21:57 -0500

    clase camara y clase ventana

---
+++ DOJO-FIGHTERS-SFML/DOJO-FIGHTERS-SFML/include/Camera.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Graphics.hpp"
+class CCamera
+{
+public:
+	CCamera();
+	~CCamera();
+	sf::View * getView() { return m_view; };
+	void setView(const float &posx, const float &posy, const float &width, const float &height);
+	void incrementZoom();
+	void decrementZoom();
+	void moveCam(const float &dirx, const float &diry, const float &dirz);
+	void initCamera(const float &posx, const float &posy, const float &width, const float &height);
+	void onDelete();
+	sf::Vector2f getPosition() { return position; };
+private:
+	sf::View * m_view;
+	float minZoom = 0.8f;
+	float maxZoom = 1.2f;
+	float moveSpeed = 0.5f;
+	sf::Vector2f position;
+	sf::Vector2f size;
+
+};
